5214480c6df95e4fac7f9274309635651b710a45
Nicolaskn95 <nicolasjap@hotmail.com>
2026-01-11 20:47:40 -0300

    feat: add Container, Header, and Menu components for layout structure

---
+++ frontend/app/shared/Header.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import { Container } from './Container'
+import Link from 'next/link'
+import { Menu } from './Menu'
+
+export function Header() {
+	return (
+		<header className="bg-red-300 flex items-center h-16">
+			<Container className="flex-1 flex justify-between items-center">
+				<div>
+					<Link href="/" className="hidden sm:block">
+						<Image src="/logo.svg" alt="Logo" width={80} height={0} />
+					</Link>
+					<Menu />
+				</div>
+				<div className="hidden sm:flex items-center">
+					<Link
+						href="https://www.linkedin.com/in/nicolas-katsuji-nagano-90ba48223/"
+						className="bg-white rounded-full px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-100"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Perfil
+					</Link>
+				</div>
+			</Container>
+		</header>
+	)
+}
